ad246ef84991a5361f16822161b16d2c5a8d6f40
Sonal Singh <sonal.singh.19993@gmail.com>
2021-03-18 05:20:47 +0000

    gram: Convert to gram specific tree
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- arrow_gram.mk
+++ arrow_gram.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from miatoll  device
-$(call inherit-product, device/xiaomi/miatoll/device.mk)
+# Inherit from gram device
+$(call inherit-product, device/xiaomi/gram/device.mk)
 
 # Inherit some common ArrowOS stuff
 $(call inherit-product, vendor/arrow/config/common.mk)
@@ -14,10 +14,10 @@ $(call inherit-product, vendor/arrow/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
-PRODUCT_DEVICE := miatoll
+PRODUCT_NAME := arrow_gram
+PRODUCT_DEVICE := gram
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Miatoll
+PRODUCT_MODEL := Poco M2 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -26,6 +26,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 DEVICE_MAINTAINER := SonalSingh18
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+    PRIVATE_BUILD_DESC="gram_in-user 11 RKQ1.200826.002 V12.5.6.0.RJPINXM release-keys"
 
-BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
+BUILD_FINGERPRINT := POCO/gram_in/gram:11/RKQ1.200826.002/V12.5.6.0.RJPINXM:user/release-keys
